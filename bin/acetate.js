be9f549049007bb9dcd56bab2f643b3c2b4380d4
Patrick Arlt <patrick.arlt@gmail.com>
2015-04-13 09:46:03 -0700

    remove ability for global aceate, update for 0.0.27

--- bin/acetate.js
+++ bin/acetate.js
@@ -3,36 +3,15 @@
 var resolve = require('resolve');
 var path = require('path');
 
-var NODE_PATH = process.env.NODE_PATH;
-
-function run(acetatePath){
-  var acetateCli = require(path.join(acetatePath, 'lib', 'cli'));
-  acetateCli.run();
-}
-
-resolve('acetate', {basedir: process.cwd()}, function(err, pathToAcetate) {
-  if (!err) {
-    return run(pathToAcetate.replace(/(\/|\\)lib(\/|\\)index\.js/, ''));
-  }
-
-  // We can't load a global one, since NODE_PATH is not defined.
-  if (!NODE_PATH) {
-    console.error('Cannot find local Acetate!\nInstall Acetate with `npm install acetate --save-dev`.\nIf you want to use a global instance, set a NODE_PATH env variable.\n');
+resolve('acetate', {basedir: process.cwd()}, function(error, pathToAcetate) {
+  if (!pathToAcetate || error) {
+    console.error('Cannot find Acetate!\nInstall Acetate with `npm install acetate --save-dev`.');
     process.exit(1);
     return;
   }
 
-  // Let's try global paths.
-  var globalPaths = NODE_PATH.split(path.delimiter);
-  var globalAcetate;
+  var acetateRoot = pathToAcetate.replace(/(\/|\\)index\.js/, '');
+  var acetateCli = require(path.join(acetateRoot, 'lib', 'cli'));
 
-  while (globalPaths.length) {
-    globalAcetate = path.normalize(path.join(globalPaths.shift(), 'acetate'));
-    if (fs.existsSync(globalAcetate)) {
-      return requireCliAndRun(globalAcetate);
-    }
-  }
-
-  console.error('Cannot find local Acetate!\nPlease install Acetate with `npm install acetate --save-dev`.\n');
-  process.exit(1);
+  acetateCli.run();
 });
