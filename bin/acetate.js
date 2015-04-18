bdfef3a04fdf7ce6d1e98b0e35f35ead5acda8e9
Patrick Arlt <patrick.arlt@gmail.com>
2015-04-17 19:06:40 -0700

    semi standard

--- bin/acetate.js
+++ bin/acetate.js
@@ -3,7 +3,7 @@
 var resolve = require('resolve');
 var path = require('path');
 
-resolve('acetate', {basedir: process.cwd()}, function(error, pathToAcetate) {
+resolve('acetate', {basedir: process.cwd()}, function (error, pathToAcetate) {
   if (!pathToAcetate || error) {
     console.error('Cannot find Acetate!\nInstall Acetate with `npm install acetate --save-dev`.');
     process.exit(1);
